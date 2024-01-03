15e5805c68cd247eccf4c880a01075417f03fffe
髙橋紀美 <xvipea0@gmail.com>
2024-01-03 23:46:17 +0900

    update

--- SwiftDataSumpleApp/Item.swift
+++ SwiftDataSumpleApp/Item.swift
@@ -7,37 +7,11 @@
 
 import Foundation
 import SwiftData
-
 @Model
 final class Item {
     var timestamp: Date
-    var message: String
-    var image: String
     
-    init(timestamp: Date, message: String, image: String) {
+    init(timestamp: Date) {
         self.timestamp = timestamp
-        self.message = ""
-        self.image = ""
     }
-}
-
-enum SampleTripsSchemaV1: VersionedSchema {
-    static var models: [any PersistentModel.Type] {
-        [Trip.self, BucketListItem.self, LivingAccommodation.self]
-    }
-
-    static var versionIdentifier: String?
-
-    @Model
-    final class Trip {
-        var name: String
-        var destination: String
-        var start_date: Date
-        var end_date: Date
-
-        var bucketList: [BucketListItem]? = []
-        var livingAccommodation: LivingAccommodation?
-    }
-
-    // Define the other models in this version...
 }
