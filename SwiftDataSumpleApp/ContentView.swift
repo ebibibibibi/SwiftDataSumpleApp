b53e0c33e3a3d023ef69e57d5a2ff0758d48cd63
髙橋紀美 <xvipea0@gmail.com>
2024-03-20 04:29:30 +0900

    レイアウトの実装

--- SwiftDataSumpleApp/ContentView.swift
+++ SwiftDataSumpleApp/ContentView.swift
@@ -8,50 +8,134 @@
 import SwiftUI
 import SwiftData
 
+struct RecordBook {
+    var title: String
+    var image: [String]
+    var createdAt: Date
+    var updatedAt: Date?
+}
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var messages: [Message]
     @State private var selection: Message?
     
     var body: some View {
-        
-        NavigationStack {
-            ZStack {
-                List {
-                    NavigationLink("子ビューに遷移", value: "CodeCandy")
-                }
-                // 子ビューに遷移
-                .navigationDestination(for: String.self) { parentValue in
-                    
-                    HogeView(value: parentValue)
-                    
-                }
+        VStack(spacing: 0) {
+            HeaderTitleView()
+            
+            ScrollView(.vertical) {
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                ScrollViewItem()
             }
-            .navigationTitle("親ビュー")
+        }
+        .background(
+            // 背景色の設定
+            LinearGradient(gradient: Gradient(colors: [Color(red: 18 / 255, green: 18 / 255, blue: 36 / 255), Color(red: 94 / 255, green: 58 / 255, blue: 102 / 255)]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+        )
+    }
+}
+
+// 画面上部　ヘッダー
+struct HeaderTitleView: View {
+    
+    var body: some View {
+        VStack (spacing: 0) {
+            Spacer()
+                .frame(height: 50)
+            HStack(spacing: 0) {
+                Text("本棚")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color(red: 55 / 255, green: 50 / 255, blue: 70 / 255))
+                    .background(
+                        Color.white
+                            .cornerRadius(90)
+                    )
+            }
+            .padding(.horizontal, 20)
         }
     }
 }
 
-/// 遷移先の子ビューでもnavigationDestinationを定義可能
-/// 親ビュー側のnavigationDestinationと指定データ型が同じである場合、意図した遷移が発火しないため注意
-struct HogeView: View {
-    
-    var value: String
-    
+
+struct ScrollViewItem: View {
+    let date = Date()
     var body: some View {
-        List {
-            NavigationLink("子ビューに遷移", value: 2023)
-        }
-        .navigationTitle("子ビュー")
-        // 子子ビューに遷移
-        .navigationDestination(for: Int.self) { childValue in
-            
-            VStack {
-                Text("\(childValue)年")
-                Text(value)
+        VStack {
+            HStack {
+                Text("これはタイトルです")
+                    .lineLimit(nil)
+                    .font(.system(.title3, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .font(.title)
-            .navigationTitle("子子ビュー")
+            
+            Spacer()
+                .frame(height: 20)
+            
+            Text("""
+これは長いテキストです
+これは長いテキストです
+これは長いテキストですこれは長いテキストです
+""")
+            .lineLimit(nil)
+            .font(.system(.body, design: .rounded))
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+                .frame(height: 20)
+            
+            Divider()
+                .frame(height: 1)
+                .background(Color(red: 95 / 255, green: 85 / 255, blue: 100 / 255))
+            
+            HStack {
+                Text(startDateToString(date))
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.gray)
+                
+                Spacer()
+                
+                Image(systemName: "ellipsis")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20)
+                    .background(Color(red: 55 / 255, green: 50 / 255, blue: 70 / 255))
+            }
+            .padding(.top, 10)
         }
+        .padding(20)
+        .background(
+            Color(red: 60 / 255, green: 60 / 255, blue: 75 / 255)
+                .opacity(0.5)
+        )
+        .cornerRadius(10)
+        .frame(minHeight: 150)
+        .padding(.horizontal, 20)
     }
+    
+    private func startDateToString(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        // ロケールを日本に設定
+        formatter.locale = Locale(identifier: "ja_JP")
+        // 曜日を含む形式で日付をフォーマット
+        formatter.dateFormat = "M月d日 EEEE"
+        
+        return formatter.string(from: date)
+    }
+
 }
